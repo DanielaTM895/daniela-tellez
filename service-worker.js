5d7f03fef848f9473117095e70f9e1433ebffabf
Daniela Tellez <daniela.tellez@utxicotepec.edu.mx>
2021-11-17 00:00:23 -0600

    Cache first y Network first

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
 var cache_daniela_tellez = "cache-v1-daniela";
 
+var urlAppShell = [
+  "index.html",
+  "LICENSE",
+  "assets/js/main.js",
+  "assets/images/loader.svg",
+];
 
 self.addEventListener("install", function (event) {
   console.log("install SW");
-
-  var urlAppShell = [
-    "index.html",
-    "LICENSE.txt",
-    "assets/css/style.css",
-    "assets/js/main.js",
-    "assets/images/loader.svg",
-  ];
 
   const _openCache = async () => {
     const _appShellStorage = await caches.open(cache_daniela_tellez);
@@ -20,3 +18,52 @@ self.addEventListener("install", function (event) {
   event.waitUntil(_openCache());
 });
 
+//1. Cache Only
+//Verifica si el cache concuerda con alguna peticion --cache-only
+/* 
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(caches.match(event.request));
+
+});
+ */
+
+// 2. Network Only
+//En este tipo de aplicacion, el cache es ignorado
+
+/* self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+}); */
+
+// 3. Cache First
+
+/*self.addEventListener("fetch", (event) => {
+    const response = caches
+   .match(event.request)
+   .then((res) => {
+     console.log("Existe el request " + event.request);
+     console.log(res);
+   })
+   .catch((res) => {
+     console.log("No existe el request " + event.request);
+     console.log(res);
+   }); 
+
+   event.respondWith(
+    caches.match(event.request).then(respuestaCache =>{
+      return respuestaCache || fetch(event.request);
+    })
+   );
+
+
+});*/
+
+// 4. Network First
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).then((respuestaRed) => {
+      return respuestaRed || caches.match(event.request);
+    })
+  );
+});
